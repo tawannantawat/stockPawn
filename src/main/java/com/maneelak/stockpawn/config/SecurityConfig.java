c4771cc64f408e619131a9b8afe8c297552e2476
tawannantawat <tawanswc@gmail.com>
2025-06-20 15:55:13 +0700

    add BCryptPasswordEncoder to security config file

--- src/main/java/com/maneelak/stockpawn/config/SecurityConfig.java
+++ src/main/java/com/maneelak/stockpawn/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.maneelak.stockpawn.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.*;
@@ -38,5 +39,10 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
